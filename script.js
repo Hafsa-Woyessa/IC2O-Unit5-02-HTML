6db8fc6fc58562de32a12ee72b7deca0b2aa71e4
HafsaWoyessa <>
2021-06-01 15:01:07 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('button').addEventListener('click', 
+displayMessage)
+
+function displayMessage () {
+  document.getElementById('paragraph').innerHTML =
+  "Hello"
+}
+
+document.getElementById('button').addEventListener
+('click', myFunction)
+function myFunction() {
+  alert ("Hello World!")
+}
+
+//This line displays a Hello message 
+alert('Hello')
+
+alert('World') // This line displays a World message
