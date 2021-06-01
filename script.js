768dbdd07ee076ceef940e20c433fe0ead3516ec
HafsaWoyessa <>
2021-06-01 15:13:39 +0000

    line 15

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-document.getElementById('button').addEventListener('click', 
-displayMessage)
+document.getElementById('button').addEventListener('click',
+  displayMessage)
 
 function displayMessage () {
   document.getElementById('paragraph').innerHTML =
-  "Hello"
+  'Hello'
 }
 
 document.getElementById('button').addEventListener
 ('click', myFunction)
-function myFunction() {
-  alert ("Hello World!")
+function myFunction () {
+  alert ('Hello World')
 }
 
-//This line displays a Hello message 
+// This line displays a Hello message
 alert('Hello')
 
 alert('World') // This line displays a World message
